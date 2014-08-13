394107c59e4e3f8fa95ff6b0807f4cf2e318a713
web object framework <xlyyc@163.com>
2014-08-13 21:19:57 +0800

    0813

--- src/wof/util/Selector.js
+++ src/wof/util/Selector.js
@@ -7,50 +7,119 @@
 if (!wof.util.Selector) {
     wof.util.Selector = {
 
-        //todo 有待完善的语法分析
-        _parser:function(queryString){
-            var selectors = [];
-            //todo 先检查是否为群组选择器
-            //todo 再检查是否为层次选择器
-            //todo 再检查是否为基本选择器
-            //todo 再检查是否包括过滤选择器(简单、内容、属性、子对象、可见性)
-
-            //以下先简单实现 仅支持基本选择器的语法
-            var queryString = jQuery.trim(queryString);
-            if(queryString!=null &&queryString.length>0){
-                if(queryString=='*'){
-                    selectors.push({type:'*',query:queryString});
-                }else if(queryString.indexOf('#')==0){ //id选择器
-                    var queryStr = queryString.substring(1);
-                    selectors.push({type:'#',query:queryStr});
-                }else if(queryString.indexOf('[')==0){ //属性选择器
-                    var queryStr = queryString.substring(1,queryString.lastIndexOf(']'));
-                    selectors.push({type:'attr',query:queryStr});
-                }else{
-                    selectors.push({type:'className',query:queryString});
-                }
-
-            }
-            return selectors;
-        },
+        _leftMatch : null,
 
         /**
-         * 对选择器语法进行解析并调用对应的选择器
+         * 获得leftMatch的正则定义
          */
-        find: function(queryString) {
-            var sls = new wof.util.SelectorList();
-            var selectors = wof.util.Selector._parser(queryString);
-            for(var i=0;i<selectors.length;i++){
-                var ss = selectors[i];
-                if(ss.type=='*'){
-                    sls = wof.util.Selector._getAllObjects();
-                }else if(ss.type=='#'){
-                    sls = wof.util.Selector._getObjectById(ss.query);
-                }else if(ss.type=='className'){
-                    sls = wof.util.Selector._getObjectByClassName(ss.query);
-                }else if(ss.type=='attr'){
-                    sls = wof.util.Selector._getObjectByAttr(ss.query);
+        _getLeftMatch: function(){
+            if(wof.util.Selector._leftMatch==null){
+                wof.util.Selector._leftMatch = {};
+                var leftMatch = {
+                    //提取id定位符
+                    ID: /#((?:[\w\u00c0-\uFFFF\-]|\\.)+)/,
+                    //提取样式定义
+                    CLASS: /\.((?:[\w\u00c0-\uFFFF\-]|\\.)+)/,
+                    //提取名称
+                    NAME: /\[name=['"]*((?:[\w\u00c0-\uFFFF\-]|\\.)+)['"]*\]/,
+                    //提取属性
+                    ATTR: /\[\s*((?:[\w\u00c0-\uFFFF\-]|\\.)+)\s*(?:(\S?=)\s*(?:(['"])(.*?)\3|(#?(?:[\w\u00c0-\uFFFF\-]|\\.)*)|)|)\s*\]/,
+                    //提取标签
+                    TAG: /^((?:[\w\u00c0-\uFFFF\*\-]|\\.)+)/,
+                    //提取子对象过滤伪类
+                    CHILD: /:(only|nth|last|first)-child(?:\(\s*(even|odd|(?:[+\-]?\d+|(?:[+\-]?\d*)?n\s*(?:[+\-]\s*\d+)?))\s*\))?/,
+                    //提取位置伪类
+                    POS: /:(nth|eq|gt|lt|first|last|even|odd)(?:\((\d*)\))?(?=[^\-]|$)/,
+                    //提取伪类
+                    PSEUDO: /:((?:[\w\u00c0-\uFFFF\-]|\\.)+)(?:\((['"]?)((?:\([^\)]+\)|[^\(\)]*)+)\2\))?/
+                };
+                //为leftMatch正则添加前缀和后缀
+                for(var type in leftMatch){
+                    leftMatch[type] = new RegExp(leftMatch[type].source+(/(?![^\[]*\])(?![^\(]*\))/.source));
+                    wof.util.Selector._leftMatch[type] = new RegExp( /(^(?:.|\r|\n)*?)/.source + leftMatch[type].source.replace(/\\(\d+)/g, function(all,num){
+                        return "\\"+(num-0 +1);
+                    }));
                 }
+            }
+            return wof.util.Selector._leftMatch;
+        },
+
+
+        //todo 尚未完全实现的语法分析器
+        //对选择器语法进行解析并调用对应的选择器
+        find:function(queryString){
+            var sls;
+            //分析区隔块表达式
+            function chunk(qStr){
+                var leftMatch = wof.util.Selector._getLeftMatch();
+                var type='className',id,className='*',pseudo,attrs=[];
+                var mm = leftMatch.ID.exec(qStr);
+                if(mm){
+                    type = 'id';
+                    id = mm[2];
+                    qStr = '';
+                }else if(mm = leftMatch.TAG.exec(qStr)){
+                    type = 'className';
+                    var className = mm[2];
+                    qStr = qStr.replace(leftMatch.TAG,'');
+                }
+                //提取伪类 如果存在伪类 则从左向右查找
+                //反之 从右向左查找
+                if(mm=leftMatch.PSEUDO.exec(qStr)){
+                    pseudo = mm[2];
+                    qStr = qStr.replace(leftMatch.PSEUDO,'');
+                }
+                //提取属性
+                while(mm=leftMatch.ATTR.exec(qStr)){
+                    var attr = mm[2];
+                    var op = mm[3];
+                    var value = mm[4]?mm[5]:mm[6];
+                    attrs.push({attr:attr,op:op,value:value});
+                    qStr = qStr.replace(leftMatch.ATTR,'');
+                }
+                return {type:type,id:id,className:className,pseudo:pseudo,attrs:attrs};
+            }
+
+            //提取区隔块表达式和块间关系符
+            var chunker = /((?:\((?:\([^()]+\)|[^()]+)+\)|\[(?:\[[^\[\]]*\]|['"][^'"]*['"]|[^\[\]'"]+)+\]|\\.|[^ >+~,(\[\\]+)+|[>+~])(\s*,\s*)?((?:.|\r|\n)*)/g;
+            //var soFar = '*,input:hidden[name^="news"][xx="tt"] input';
+            var m, soFar=queryString, parts=[], extra;
+            do{
+                chunker.exec('');
+                m = chunker.exec(soFar);
+                if(m){
+                    soFar = m[3];
+                    parts.push(m[1]);
+                    if (m[2]) {
+                        extra = m[3];
+                        break; //todo 目前不支持群组选择
+                    }
+                }
+            }while(m);
+            //获取区隔块表达式
+            if(parts.length==1){
+                var qs = chunk(parts[0]);
+                console.log(JSON.stringify(qs));
+                //如果type是id 则直接按照id查找
+                if(qs['type']=='id'){
+                    sls = wof.util.Selector._getObjectById(ss.query);
+                }else{
+                    if(qs['pseudo']){
+                        console.log('从左向右');
+                        if(qs['type']=='className'){
+                            if(qs['className']=='*'){
+                                sls = wof.util.Selector._getAllObjects();
+                                console.log('111111==========='+sls.size());
+                            }
+                            //todo 根据伪类过滤
+                        }
+                    }else{
+                        console.log('从右向左');
+                    }
+                }
+            }else if(parts.length>1){
+                //todo 目前还不支持层次选择
+
             }
             return sls;
         },
@@ -75,6 +144,7 @@ if (!wof.util.Selector) {
         _getAllObjects: function() {
             var sls = new wof.util.SelectorList();
             var oIds = wof.util.ObjectManager.oIds();
+            alert(oIds.length);
             for(var i=0;i<oIds.length;i++){
                 var obj = wof.util.ObjectManager.get(oIds[i]);
                 if(obj!=null&&obj.getIsComponent()==true){
